50a91942e4127efb72f348588ed5addb1118cc47
송영도 <79524109+0csong@users.noreply.github.com>
2022-05-31 15:30:07 +0900

    Update main(tflite).py

--- Signlanguage/main(tflite).py
+++ Signlanguage/main(tflite).py
@@ -55,17 +55,7 @@ while cap.isOpened():
             # Inference gesture
             data = np.array([angle], dtype=np.float32)
 
-            # Gesture recognition model
-            # interpreter = tf.lite.Interpreter(
-            #     model_path='model/handtrain(400(91)).tflite',
-            #     num_threads=1)
-            # interpreter.allocate_tensors()
-            # input_details = interpreter.get_input_details()
-            # output_details = interpreter.get_output_details()
-            # interpreter.set_tensor(input_details[0]['index'], data)
-            # interpreter.invoke()
-            # output_data = interpreter.get_tensor(output_details[0]['index'])
-            # result=np.squeeze(output_data)
+           
             result=interpre(data)
             idx = np.argmax(result)
 
